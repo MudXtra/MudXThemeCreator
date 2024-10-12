42ab7aaaec9226723b54253edd685c6114eea8bd
Versile Johnson II <versilej@gmail.com>
2024-10-11 23:42:18 -0500

    Added a ChangeLogItem model for when application needs updates. Updated About page to use Changelog and put in the next two changes.

---
+++ ThemeCreatorMudBlazor.DAL/Models/ChangeLogItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThemeCreatorMudBlazor.DAL.Models
+{
+    public record ChangeLogItem(DateTime ChangeWhen, string ChangeWhat, string ChangeWhy, string ChangeWho)
+    {
+    }
+}
