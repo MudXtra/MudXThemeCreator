d1a353144790f82a1616ea5e8e15b6f2454bfb85
Versile Johnson II <versilej@gmail.com>
2025-03-07 11:28:09 -0600

    https force

--- src/MudBlazorThemes.UI/Program.cs
+++ src/MudBlazorThemes.UI/Program.cs
@@ -97,6 +97,18 @@ builder.Services.AddAuth0WebAppAuthentication(options =>
 
 var app = builder.Build();
 
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedProto |
+                       ForwardedHeaders.XForwardedHost
+});
+
+app.UseHttpsRedirection();
+app.MapStaticAssets();
+app.UseAntiforgery();
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapGet("/Account/Login", async (HttpContext httpContext, string returnUrl = "/") =>
 {
     var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
@@ -117,13 +129,6 @@ app.MapGet("/Account/Logout", async (HttpContext httpContext) =>
     await httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
 });
 
-app.UseHttpsRedirection();
-app.MapStaticAssets();
-app.UseHttpsRedirection();
-app.UseAntiforgery();
-app.UseAuthentication();
-app.UseAuthorization();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -131,11 +136,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-app.UseForwardedHeaders(new ForwardedHeadersOptions
-{
-    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto | ForwardedHeaders.XForwardedHost
-});
 
 // Add headers
 app.Use(async (context, next) =>
